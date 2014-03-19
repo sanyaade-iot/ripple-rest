88b59eadc6c3312a09d8b2762b83e7e6f85c8a40
David Wee <rook2pawn@gmail.com>
2014-03-18 18:27:10 -0700

    trying to trigger travis

--- test/unit-usecase1.js
+++ test/unit-usecase1.js
@@ -8,6 +8,7 @@ var log = function(obj) {
     console.log(util.inspect(obj, { showHidden: true, depth: null }));
 };
 var PEOPLE = {};
+
 PEOPLE.rook2pawn = 'rwUNHL9AdSupre4tGb7NXZpRS1ift5sR7W';
 PEOPLE.rook2pawn_gw = 'rpzgG7yxjEP9EHf2roftLvPTvt4wfL3iYY';
 PEOPLE.rook2pawn_mm = 'rpXF7z1sypK41CFhFzNHczSE47w8rGLx7W';
