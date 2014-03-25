3a52ebad63bd4f17c81154f74c300548ba5033ef
David Wee <rook2pawn@gmail.com>
2014-03-25 15:32:15 -0700

    added cold wallet for gateway

--- test/testconfig.js
+++ test/testconfig.js
@@ -18,6 +18,7 @@ people.rook2pawn = 'rwUNHL9AdSupre4tGb7NXZpRS1ift5sR7W';
 people.rook2pawn_gw = 'rpzgG7yxjEP9EHf2roftLvPTvt4wfL3iYY';
 people.rook2pawn_mm = 'rpXF7z1sypK41CFhFzNHczSE47w8rGLx7W';
 people.rook2pawn_receiver = 'rp4GSjosE4TrPvmmfxFhu2Awf7BQn4dQoH';
+people.rook2pawn_gw_cold = 'r97EYvF42JJdEFF3qaXPnDC2C7gP7Ar42Q';
 exports.people = people;
 
 var secrets = {};
@@ -25,4 +26,5 @@ secrets.rook2pawn = process.env.SECRET_SENDER || 'shDiLVUXYGFDCoMDP6HfHnER5dpmP'
 secrets.rook2pawn_gw = process.env.SECRET_GW || 'ssR6VukdxALxWdbPrRn79qmcQ7jUp';
 secrets.rook2pawn_mm = process.env.SECRET_MM || 'sasik63mHZbFnqMVY4E8dgzVwRbE7';
 secrets.rook2pawn_receiver = process.env.SECRET_RECEIVER || 'shCASB7uWpt5jnX8EuU3BZh1ZQxVP';
+secrets.rook2pawn_gw_cold = process.env.SECRET_GW_COLD || 'snhw3b4GFrk57EacGg4Gc6j7zAhEw';
 exports.secrets = secrets;
